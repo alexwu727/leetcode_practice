272a817e9443e3722dac597c4ec0e7ced762c858
Alex Wu <alexwu727@gmail.com>
2021-04-12 15:10:16 +0800

    solve problem 4

---
+++ array/4MedianOfTwoSortedArrays.py
@@ -0,0 +1,24 @@
+from problem_abc import ProblemAbc
+
+
+class Problem1(ProblemAbc):
+    def __init__(self):
+        self.url = "https://leetcode.com/problems/median-of-two-sorted-arrays/"
+        self.test_cases = [([1, 3], [2]), ([1, 2], [3, 4]), ([0, 0], [0, 0]), ([], [1]), ([2], [])]
+        self.y = [2, 2.5, 0, 1, 2]
+
+    @staticmethod
+    def solution(test_case):
+        nums1 = test_case[0]
+        nums2 = test_case[1]
+
+        nums = sorted(nums1 + nums2)
+        if len(nums) % 2 == 0:
+            return (nums[len(nums) // 2] + nums[len(nums) // 2 - 1]) / 2
+        else:
+            return nums[len(nums) // 2]
+
+
+if __name__ == '__main__':
+    p = Problem1()
+    p.submit()
