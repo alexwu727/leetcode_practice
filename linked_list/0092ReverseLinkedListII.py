936b9b7a3a932ae9a65f25857d9ffb913ff8f34d
Alex Wu <alexwu727@gmail.com>
2021-04-28 09:57:27 +0800

    solved problem 92

---
+++ linked_list/0092ReverseLinkedListII.py
@@ -0,0 +1,43 @@
+from problem_abc import ProblemAbc
+from nodes import list_to_nodes
+from nodes import nodes_to_list
+
+
+class Problem92(ProblemAbc):
+    def __init__(self):
+        self.url = "https://leetcode.com/problems/reverse-linked-list-ii/"
+        self.test_cases = [([1, 2, 3, 4, 5], 2, 4),
+                           ([1, 2, 3, 4, 5], 1, 4),
+                           ([1, 2, 3, 4, 5], 2, 5),
+                           ([1, 2, 3, 4, 5], 1, 5)]
+        self.y = [[1, 4, 3, 2, 5], [4, 3, 2, 1, 5], [1, 5, 4, 3, 2], [5, 4, 3, 2, 1]]
+        self.adjust_output = nodes_to_list
+
+    @staticmethod
+    def solution(test_case):
+        head = list_to_nodes(test_case[0])
+        left = test_case[1]
+        right = test_case[2]
+
+        if left == right:
+            return head
+        cur = head
+        l = []
+        while cur:
+            l.append(cur)
+            cur = cur.next
+        for i in range(left, right):
+            l[i].next = l[i - 1]
+        if right == len(l):
+            l[left - 1].next = None
+        else:
+            l[left - 1].next = l[right]
+        if left != 1:
+            l[left - 2].next = l[right - 1]
+            return head
+        return l[right - 1]
+
+
+if __name__ == '__main__':
+    p = Problem92()
+    p.submit()
