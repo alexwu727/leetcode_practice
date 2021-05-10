0a3aff9e48e299d09c7cf62c5e6cff0de771af8e
Alex Wu <alexwu727@gmail.com>
2021-05-10 13:56:16 +0800

    modified the solution of problem 5

--- string/0005LongestPalindromicSubstring.py
+++ string/0005LongestPalindromicSubstring.py
@@ -13,16 +13,21 @@ class Problem5(ProblemAbc):
 
         if len(s) == 1:
             return s
-        mx = 1 if s[0] != s[1] else 2
-        ans = s[0] if s[0] != s[1] else s[0:2]
+        mx = 1
+        ans = s[0]
         i = (len(s) - 1) // 2
         n = 1
         direction = 1
-        while (i > 0) and (i < len(s) - 1):
+        # start from the middle
+        # the edge character is restricted
+        # e.g. abcde : c -> d -> b -> a -> e
+        while (i >= 0) and (i <= len(s) - 1):
+            # if ith length of maximum substring is less than mx
             if (i + 1) * 2 <= mx:
                 break
             if (len(s) - i) * 2 - 1 < mx:
                 break
+            # center of string
             for j in [1, 2]:
                 left = i
                 right = i + j
@@ -36,6 +41,8 @@ class Problem5(ProblemAbc):
                         right += 1
                     else:
                         break
+
+            # change the direction for the next iteration
             i += n * direction
             direction = -direction
             n += 1
