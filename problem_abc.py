57e20c575bd3dbf681ddc36c9e89a95bc03d8323
Alex Wu <alexwu727@gmail.com>
2021-06-24 17:39:19 +0800

    modified the verify method for question with any-order type solution

--- problem_abc.py
+++ problem_abc.py
@@ -6,6 +6,7 @@ class ProblemAbc(ABC):
     test_cases = []
     y = []
     adjust_output = False
+    wrong = False
 
     @abstractmethod
     def solution(self):
@@ -15,15 +16,28 @@ class ProblemAbc(ABC):
         print("problem url:", self.url)
         for i in range(len(self.test_cases)):
             output = self.solution(self.test_cases[i])
-            if self.adjust_output:
-                output = self.adjust_output(output)
-            if output != self.y[i]:
-                print("===== wrong output ======")
-                print("------- test case -------")
-                print(self.test_cases[i], "\n")
-                print("---- expected output ----")
-                print(self.y[i])
-                print("------ your output ------")
-                print(output)
-                return
+            if self.adjust_output == "any_order":
+                for j in output:
+                    if j not in self.y[i]:
+                        self.wrong = True
+                        break
+                for j in self.y[i]:
+                    if j not in output:
+                        self.wrong = True
+                        break
+
+            else:
+                if self.adjust_output:
+                    output = self.adjust_output(output)
+                if output != self.y[i]:
+                    self.wrong = True
+        if self.wrong:
+            print("===== wrong output ======")
+            print("------- test case -------")
+            print(self.test_cases[i], "\n")
+            print("---- expected output ----")
+            print(self.y[i])
+            print("------ your output ------")
+            print(output)
+            return
         print("valid solution!")
